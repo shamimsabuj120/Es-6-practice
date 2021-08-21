23dd744712465ea317c64642bca6ef3443cfe9b8
shamim sabuj <76744826+shamimsabuj120@users.noreply.github.com>
2021-08-21 20:47:53 +0600

    first day done

---
+++ letConst.js
@@ -0,0 +1,17 @@
+const numbers = [12,43];
+numbers[1]=88;
+numbers.push(12);
+console.log(numbers);
+const nayok ={name:"sakib kahn", phone:"01724305107"}
+console.log(nayok);
+
+let patientName ="rahim chaca";
+patientName="fatema khala"
+console.log(patientName);
+
+let sum =0;
+for(let i=0;i<10;i++){
+    sum =sum+i;
+    console.log(i);
+}
+

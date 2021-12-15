85b2c215c7b6042de5fd49d3502ec142d3a4b64d
shamim sabuj <76744826+shamimsabuj120@users.noreply.github.com>
2021-12-15 13:28:00 +0600

    new addition

--- callApply.js
+++ callApply.js
@@ -1,16 +1,56 @@
-const normalPerson={
-    firstName:"shamim",
-    lastNAme:"sabuj",
-    salary:15000,
-    gateFullName:function(){
-        console.log(this.firstName, this.lastNAme);
-    },
-    billCharge:function(amount){
-        this.salary=this.salary-amount;
-        return this.salary;
+// const normalPerson={
+//     firstName:"shamim",
+//     lastNAme:"sabuj",
+//     salary:15000,
+//     gateFullName:function(){
+//         console.log(this.firstName, this.lastNAme);
+//     },
+//     billCharge:function(amount, tips){
+//         console.log(this);
+//         this.salary=this.salary-amount-tips;
+//         return this.salary;
+//     }
+// }
+// // normalPerson.billCharge(3000);
+// // normalPerson.billCharge(6000);
+// // console.log(normalPerson.salary);
+// // console.log(normalPerson.lastNAme); 
+
+// const heroPerson={
+//     firstName:'hero',
+//     lastName:'Golam',
+//     salary:2500
+// }
+// const friendlyPerson={
+//     firstName:'hero',
+//     lastName:'Balam',
+//     salary:2500
+// }
+
+// // normalPerson.billCharge()
+
+// // const heroChargeBill=normalPerson.billCharge.bind(heroPerson);
+// // heroChargeBill(2000)
+// // heroChargeBill(200)
+// // console.log(heroPerson.salary)
+// // console.log(normalPerson.salary)
+
+// // normalPerson.billCharge.call(heroPerson, 900);
+// // normalPerson.billCharge.call(heroPerson, 3000);
+// // console.log(heroPerson.salary)
+// // normalPerson.billCharge.call(friendlyPerson, 600, 100)
+// // console.log(friendlyPerson.salary)
+// // console.log(normalPerson.salary) 
+// normalPerson.billCharge.apply(friendlyPerson, 900,)
+// console.log(friendlyPerson.salary)
+
+class person{
+    constructor(firstName,lastName,salary){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.salary = salary;
     }
 }
-normalPerson.billCharge(3000);
-normalPerson.billCharge(6000);
-console.log(normalPerson.salary);
-console.log(normalPerson.lastNAme);
+
+const HeroPerson = new person('balam', 'sanam', 2500)
+console.log(HeroPerson)
---
+++ timeout.js
@@ -0,0 +1,17 @@
+function doSomething(){
+    console.log(333333);
+}
+
+console.log(12222);
+setInterval(() => {
+    console.log('doing it')
+},2000);
+// setTimeout(()=>{
+//     console.log('see you soon')
+// },5000)
+// setTimeout(function(){
+//     console.log(7777)
+// },4000)
+// setTimeout(doSomething, 4000)
+// doSomething();
+console.log(444444);
